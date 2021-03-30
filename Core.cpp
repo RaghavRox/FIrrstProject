ecba27bd25b52cea0111187c1c92281c6e8d0a62
RaghavRox <raghavrox2002@gmail.com>
2021-03-30 18:27:10 +0530

    some progress

--- Core.cpp
+++ Core.cpp
@@ -1,1 +1,49 @@
 #include"Core.h"
+
+int sudoku[9][9];
+
+bool is_ok(int arr[9][9] , int i, int j, int k) {
+    {
+        for (int temp = 0; temp < 9; temp++) {
+            if (arr[i][temp] == k || arr[temp][j] == k)
+                return false;
+        }
+        int  boxHor = (i / 3) * 3;
+        int boxVer = (j / 3) * 3;
+        for (int a = boxHor; a < boxHor + 3; a++) {
+            for (int b = boxVer; b < boxVer + 3; b++) {
+                if (arr[a][b] == k)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+
+void solver() {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            if (sudoku[i][j] == 0) {
+                for (int k = 1; k <= 9; k++) {
+                    if (is_ok(sudoku, i, j, k)) {
+                        sudoku[i][j] = k;
+                        solver();
+                        sudoku[i][j] = 0;
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
+
+
+void take_input_sudoku(int barre[9][9]) {
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            sudoku[i][j] = barre[i][j];
+        }
+    }
+}
--- Core.h
+++ Core.h
@@ -1,1 +1,4 @@
 #pragma once
+bool is_ok();
+void solver() {
+void take_input_sudoku(int barre[9][9]);
